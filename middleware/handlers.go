277a1f460557044b18cfffad7df88b0ff63bf40b
Ilyas Rufai <rufaiilyas@gmail.com>
2024-04-03 16:03:00 +0100

    update

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -1,10 +1,39 @@
 package middleware
 
-import(
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 )
 
 type response struct {
-	ID int64 `json:"id,omitempty"`
+	ID      int64  `json:"id,omitempty"`
 	Message string `json:"message,omitempty"`
+}
+
+// Creating connection to the DB server
+func CreateConnection() *sql.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// If no error returned
+	fmt.Println("Connection to postgres db established")
+	return db
 }
