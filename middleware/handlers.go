67da33078f966e47692973f8dd2b711e39d1e4b9
Ilyas Rufai <rufaiilyas@gmail.com>
2024-04-04 15:27:03 +0100

    feat: add update func

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -8,8 +8,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+
+	"github.com/GO_STOCK_API_POSTGRES/models"
 )
 
 type response struct {
@@ -19,6 +23,7 @@ type response struct {
 
 // Creating connection to the DB server
 func CreateConnection() *sql.DB {
+
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env file")
@@ -53,23 +58,69 @@ func CreateStock(w http.ResponseWriter, r *http.Request) {
 	insertID := insertStock(stock)
 
 	res := response{
-		ID: insertID,
+		ID:      insertID,
 		Message: "stock created successfully",
 	}
 
 	json.NewDecoder(w).Encode(res)
 }
 
-func GetStock() {
+func GetStock(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unabale to convert the string into int. %v", err)
+	}
+
+	stock, err := getStock(int64(id))
+
+	if err != nil {
+		log.Fatalf("UNable to get stock %v", err)
+	}
+
+	json.NewEncoder(w).Encode(stock)
+}
+
+func GetAllStock(w http.ResponseWriter, r *http.Request) {
+	stocks, err := getAllStocks()
+
+	if err != nil {
+		log.Fatalf("Unable to get all stock %v", err)
+	}
+
+	json.NewEncoder(w).Encode(stocks)
 
 }
 
-func GetAllStock() {
+func UpdateStock(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 
-}
+	id, err := strconv.Atoi(params["id"])
 
-func UpdateStock() {
+	if err != nil {
+		log.Fatalf("Unable to convert the string to an integer value: %v", err)
+	}
 
+	var stock models.Stock
+
+	err = json.NewDecoder(r.Body).Decode(&stock)
+
+	if err != nil{
+		log.Fatalf("Unable to decode the request body: %v", err)
+	}
+
+	updatedRows := updateStock(int64(id), stock)
+
+	msg := fmt.Sprintf("Stock updated successfully, Totals rows/records affected: %v", updatedRows)
+
+	res := response{
+		ID: int64(id),
+		Message: msg,
+	}
+
+	json.NewDecoder(w).Encode(res)
 }
 
 func DeleteStock() {
